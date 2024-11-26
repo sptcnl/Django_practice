c5e3be9e84d2e7e53c3adcf7a4eb58f904239baf
nyong <sptcnl@gmail.com>
2024-11-26 23:58:26 +0900

    update: model field 추가(성별, 프로필 사진, 자기소개, 생일)

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    GENDER_CHOICE = (
+        ('M', 'Man'),
+        ('W', 'Woman'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE)
+    profile_picture = models.ImageField(upload_to='profile_picture', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    birth = models.DateField(blank=True, null=True)
