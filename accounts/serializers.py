33eda88190943c83c0b2d7b06bd2b621fa88a4c1
sptcnl <sptcnl@gmail.com>
2024-11-27 14:02:28 +0900

    update: 회원가입 기능 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,16 +1,51 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 from accounts.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            'username', 
-            'first_name',
-            'last_name',
-            'email', 
-            'gender', 
-            'profile_picture', 
-            'bio', 
-            'birth',
+class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        max_length=150, 
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
         )
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    gender = serializers.ChoiceField(choices=(
+                                ('M', 'Man'),
+                                ('W', 'Woman'),
+                                ('O', 'Other'),
+                                )
+                            )
+    profile_picture = serializers.ImageField(required=False)
+    bio = serializers.CharField(required=False)
+    birth = serializers.DateField(required=False)
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {'password': '비밀번호가 일치하지 않습니다.'}
+            )
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            email = validated_data['email'],
+            gender = validated_data['gender'],
+            profile_picture = validated_data['profile_picture'],
+            bio = validated_data['bio'],
+            birth = validated_data['birth'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from accounts.serializers import SignupSerializer
 
-def signup():
-    pass
+@api_view(['POST'])
+def signup(request):
+    serializer = SignupSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
