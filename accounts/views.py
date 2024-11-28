22494358ee9d3cb4ad65443637d3aeb5a451149e
sptcnl <sptcnl@gmail.com>
2024-11-28 15:42:51 +0900

    update: 로그인 기능 수정(아직 작동X), csrf 토큰 발급 기능 추가(자꾸 없다고 에러나서 추가함) .accepted_renderer not set on Response 에러남

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.serializers import SignupSerializer
+from django.contrib.auth import login
+from django.middleware.csrf import get_token
 
-from accounts.serializers import SignupSerializer, LoginSerializer
+@api_view(['GET'])
+def get_csrftoken(request):
+    csrf_token = get_token(request)
+    return Response(csrf_token)
 
 @api_view(['POST'])
 def signup(request):
@@ -13,8 +22,20 @@ def signup(request):
 
 def login(request, how):
     print(how)
-    serializer = LoginSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
+    login_ways = ['token', 'session']
+    if how != login_ways:
+        return Response({'error': how + '의 url은 지원하고 있지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'message': '아이디 또는 비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+    
     if how == 'token':
-        token = serializer.validated_data
-        return Response({"token": token.key}, status=status.HTTP_200_OK)
+        refresh = RefreshToken.for_user(user)
+        return Response({'refresh_token': str(refresh),
+                        'access_token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+    elif how == 'session':
+        login(request, **request.data)
+        return Response({'message': '세션 로그인 성공'}, status=status.HTTP_200_OK)
