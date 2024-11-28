4b3dbd1ec56f24f8b89aefbb2e148de34ea6b167
sptcnl <sptcnl@gmail.com>
2024-11-28 18:38:54 +0900

    update: logout 기능 추가(아직 작동X)

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from accounts.serializers import SignupSerializer
-from django.contrib.auth import login as session_login
+from django.contrib.auth import (
+                                login as session_login,
+                                logout as session_logout,
+                                )
 
 @api_view(['POST'])
 def signup(request):
@@ -32,3 +36,21 @@ def login(request, how):
     elif how == 'session':
         session_login(request, user)
         return Response({'message': '세션 로그인 성공'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request, how):
+    login_ways = ['token', 'session']
+    if how not in login_ways:
+        return Response({'error': how + '의 url은 지원하고 있지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if how == 'token':
+        refresh = request.data.get("refresh")
+        if not refresh:
+            return Response({"message": "refresh token 필요"}, status=status.HTTP_400_BAD_REQUEST)
+        token = RefreshToken(refresh)
+        token.blacklist()
+        return Response({'message': '토큰 로그아웃 성공'}, status=status.HTTP_200_OK)
+    elif how == 'session':
+        session_logout(request)
+        return Response({'message': '세션 로그아웃 성공'}, status=status.HTTP_200_OK)
